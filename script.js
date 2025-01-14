379701a72ec70b4bc7f5cbde0b45dc8670b25bec
cjohnson315 <caitlinjohnson315@gmail.com>
2025-01-14 13:24:33 -0600

    first iteration satellite loop

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function getSounding() {
     let pic = document.getElementById("sounding")
     pic.src = soundingURL
 }
+
+function getSatellite() {
+    const date = new Date().toJSON()
+    let month = Number(date.slice(5,7))
+    let monthAdjusted = (month-1).toString()
+
+    let minuteCurrent = Number(date.slice(14,16))
+    let minuteChoices = [56, 51, 46, 41, 36, 31, 26, 21, 16, 11, 6, 1]
+    let minute = 0
+    
+    for (choice of minuteChoices) {
+        if(choice <= minuteCurrent) {
+            minute = choice.toString()
+            break
+        }  
+        else {
+            minute = "56"
+        } 
+    }
+    if (minute.length<2) minute = "0"+minute
+
+    let timestamp = date.slice(2,4) + monthAdjusted + date.slice(8,10) + date.slice(11,13) + minute
+
+    let satelliteURL = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/" + timestamp + "_GOES16-ABI-CONUS-GEOCOLOR-5000x3000.jpg"
+
+    console.log(satelliteURL)
+}
